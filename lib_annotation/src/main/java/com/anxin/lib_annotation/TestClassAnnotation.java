2cc7ac89bd53ce96cdbb4484a521613602521e3c
anxinxu <709701187@qq.com>
2018-02-02 18:57:39 +0800

    new test source,class,runtime annotation

---
+++ lib_annotation/src/main/java/com/anxin/lib_annotation/TestClassAnnotation.java
@@ -0,0 +1,16 @@
+package com.anxin.lib_annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by anxin on 2018/2/2.
+ * <p>
+ */
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.TYPE)
+public @interface TestClassAnnotation {
+}
---
+++ lib_annotation/src/main/java/com/anxin/lib_annotation/test/TestAnnotationClass.java
@@ -0,0 +1,12 @@
+package com.anxin.lib_annotation.test;
+
+import com.anxin.lib_annotation.TestSourceAnnotation;
+
+/**
+ * Created by anxin on 2018/2/2.
+ * <p>
+ */
+
+@TestSourceAnnotation
+public class TestAnnotationClass {
+}
